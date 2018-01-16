7f2f1db2c7aa83b2ac326c0975c7ce91dbe9d01f
Gdula <da.gdula@gmail.com>
2018-01-16 10:32:58 +0100

    added shout

--- gittie.py
+++ gittie.py
@@ -4,7 +4,8 @@ def joke():
     print("Joke joke joke joke joke")
 def add(a, b):
     return a + b
-
+def shout():
+    print("WHAT THE FUCK GIT?")
 print("The sum of {} and {} is {}".format(5, 2, add(a,b)))
 introduce()
 joke()
